b3cd1dfa382c5cba4751a14526b0ad5c705ffa56
steftes99 <stefanotescione83@gmail.com>
2021-05-31 16:46:47 +0200

    push correzione

--- cognomi/js/script.js
+++ cognomi/js/script.js
@@ -1,1 +1,13 @@
-alert('ciao')
+var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+
+var cognomeUtente = prompt('Qual è il tuo cognome?');
+
+cognomi.push(cognomeUtente.toUpperCase());
+
+document.getElementById('lista').innerHTML = cognomi;
+
+cognomi.sort();
+
+document.getElementById('lista-ordinata').innerHTML = cognomi;
+
+document.getElementById('posizione').innerHTML = cognomi.indexOf(cognomeUtente);
